917a6dedcd61364c2d96b0cb05ab2dd743dd98be
Washington <lepex102000@gmail.com>
2024-12-17 20:07:02 -0300

    feat: modificando cenário de homme

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Script de funções para
 
 // Seleciona os elementos necessários
-const input = document.getElementById("input");
-const productCards = document.querySelectorAll(".product-card");
+const input = document.getElementById('input');
+const productCards = document.querySelectorAll('.product-card');
 
 // Adiciona o evento de entrada (input) para capturar as alterações no campo de busca
-input.addEventListener("input", () => {
+input.addEventListener('input', () => {
   const searchTerm = input.value.toLowerCase(); // Captura o termo de busca em minúsculas
 
   productCards.forEach((card) => {
     const productInfo = card.textContent.toLowerCase(); // Obtém o texto do cartão
-    if (searchTerm === "" || productInfo.includes(searchTerm)) {
-      card.style.display = ""; // Mostra o cartão se corresponder ao termo de busca
+    if (searchTerm === '' || productInfo.includes(searchTerm)) {
+      card.style.display = ''; // Mostra o cartão se corresponder ao termo de busca
     } else {
-      card.style.display = "none"; // Esconde o cartão se não corresponder
+      card.style.display = 'none'; // Esconde o cartão se não corresponder
     }
   });
 });
@@ -21,23 +21,23 @@ input.addEventListener("input", () => {
 // Função para limpar o campo de entrada e exibir todos os produtos
 function Clear(inputId) {
   const inputField = document.getElementById(inputId);
-  inputField.value = ""; // Limpa o texto no campo de entrada
+  inputField.value = ''; // Limpa o texto no campo de entrada
   productCards.forEach((card) => {
-    card.style.display = ""; // Mostra todos os cartões novamente
+    card.style.display = ''; // Mostra todos os cartões novamente
   });
 }
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // Identifica os botões de navegação
-  const leftArrow = document.getElementById("left-arrow");
-  const rightArrow = document.getElementById("right-arrow");
+  const leftArrow = document.getElementById('left-arrow');
+  const rightArrow = document.getElementById('right-arrow');
 
   // Evento para redirecionar para a página "Início" ao clicar na seta para a esquerda
-  leftArrow.addEventListener("click", () => {
-    window.location.href = "HomeApp.html"; // Substitua pelo caminho correto
+  leftArrow.addEventListener('click', () => {
+    window.location.href = 'HomeApp.html'; // Substitua pelo caminho correto
   });
 
   // Evento para redirecionar para a página "Serviços" ao clicar na seta para a direita
-  rightArrow.addEventListener("click", () => {
-    window.location.href = "Verificacao.html"; // Substitua pelo caminho correto
+  rightArrow.addEventListener('click', () => {
+    window.location.href = 'Verificacao.html'; // Substitua pelo caminho correto
   });
 });
